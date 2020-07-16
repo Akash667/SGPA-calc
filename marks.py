3efb9dbcb824c149af9f66ea1fb8b10806b41c1d
Akash667 <akashrawatcats@gmail.com>
2020-07-16 17:46:39 +0530

    1.1

--- marks.py
+++ marks.py
@@ -8,6 +8,9 @@ for i in range(n):
     score = input().split()
     credit[int(score[0])] = score[1]
 
+reference = [chr(i) for i in range(97,103)]
+reference_mark = list(range(9,0,-2))
+
 
 total_credits = sum(credit.keys())
 
@@ -22,14 +25,17 @@ for k, v in credit.items():
     else:
         plus = False
 
-    difference = ord(value[0])  - ord("a")
+    for i in reference:
+        if value[0] == i:
+            index = reference.index(i)
+            break
 
-    temp_score = 10 - difference
+    temp_score = k*reference_mark[index]
 
-    if not plus:
-        temp_score-=1
+    if plus:
+        temp_score+=1
 
-    result.append(temp_score*k)
+    result.append(temp_score)
         
 sgpa = sum(result)/total_credits
 
