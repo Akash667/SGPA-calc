7ee9e6b99de0fa49a4505eeb29dd90aecc10f23b
Akash667 <akashrawatcats@gmail.com>
2020-07-16 19:17:59 +0530

    1.2

--- marks.py
+++ marks.py
@@ -1,47 +1,59 @@
-
-n = int(input("Number of subs: "))
-
 result = []
+credit = []
 
-print("credits grade: example 1 a")
 
-credit = dict()
-for i in range(n):
-    score = input().split()
-    credit[float(score[0])] = score[1]
+file_handle = open("marks_list.txt")
 
-reference = [chr(i) for i in range(97,103)]
+lines = file_handle.readlines()
+
+n = len(lines)
+
+
+for i in lines:
+    try:
+        score = i.split()
+        if score[1] == "None" or score[1] == "Qualified":
+            continue
+        score[0] = float(score[0])
+        credit.append(score)
+    except:
+        n-=1
+        pass
+
+reference = [chr(i) for i in range(97,102)]
+
 reference_mark = list(range(9,0,-2))
 
-
-total_credits = sum(credit.keys())
+total_credits = sum([i[0] for i in credit])
+print(total_credits)
 
 plus = False
 
-for k, v in credit.items():
+for k, v in credit:
 
     value = v.lower()
 
     if len(value) == 2:
         plus = True
-    else:
+    elif len(value) == 1:
         plus = False
 
     for i in reference:
         if value[0] == i:
             index = reference.index(i)
             break
-    else:
-        index = 0
-        plus = True
 
-    temp_score = k*reference_mark[index]
+    temp_score = reference_mark[index]
 
     if plus:
-        temp_score+=1
+        temp_score += 1
+    
+    temp_score *= k
 
     result.append(temp_score)
-        
+
+print(result)
+
 sgpa = sum(result)/total_credits
 
 print(sgpa)
